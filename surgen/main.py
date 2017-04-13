635e372e151dc9c9d0dfd4fa739a7d4c335baf8e
Brendan Almonte <brendana@zillowgroup.com>
2017-04-12 22:43:08 -0700

    better result tracking

--- surgen/main.py
+++ surgen/main.py
@@ -1,13 +1,14 @@
 """surgen, a codebase upgrade tool
 
 Usage:
-  surgen [<procedure_dir> <target_dir> -v]
+  surgen [-i -v <procedure_dir> <target_dir>]
 
 Options:
-  <procedure_dir>  the directory containing surgen procedure files [default: ./procedures/]
-  <target_dir>  the directory to operate on. [default: .]
-  -v, --verbose     show verbose output
-  -h, --help        show this usage guide
+  <procedure_dir>         the directory containing surgen procedure files [default: ./procedures/]
+  <target_dir>            the directory to operate on. [default: .]
+  -i, --ignore-errors     continues if a procedure fails
+  -v, --verbose           show verbose output
+  -h, --help              show this usage guide
 """
 import os
 import docopt
@@ -22,4 +23,5 @@ def main(argv=sys.argv[1:]):
     options = docopt.docopt(__doc__,  argv=argv, options_first=True)
     level = logging.DEBUG if options["--verbose"] else logging.INFO
     s = surgen_from_directory(options.get("<procedure_dir>") or DEFAULT_PROCEDURE_DIR)
-    return s.operate(options.get("<target_dir>") or DEFAULT_TARGET_DIR)
+    return s.operate(options.get("<target_dir>") or DEFAULT_TARGET_DIR,
+                     options["--ignore-errors"])
--- surgen/surgen.py
+++ surgen/surgen.py
@@ -1,6 +1,7 @@
 import os
-from collections import OrderedDict
+from collections import OrderedDict, defaultdict
 from clint.textui import colored, puts, indent
+from enum import Enum
 from .procedure import from_file
 from .exceptions import ProcedureNotFound
 
@@ -8,29 +9,41 @@ import logging
 
 LOG = logging.getLogger(__name__)
 
+class Result(Enum):
+    PASS = 0
+    FAIL = 1
+    SKIP = 2
+
 class Surgen(object):
     """ the main context to perform operations. """
 
     def __init__(self, procedures_by_name):
         self._procedures_by_name = procedures_by_name
 
-    def operate(self, target_dir):
+    def operate(self, target_dir, ignore_errors):
         """
         perform the operation on the target directory.
         return an exit code.
         """
         puts("Perfoming procedures on {0}".format(target_dir))
-        successful_procedures = 0
+        results = defaultdict(int)
         with indent(2):
             for name, procedure_cls in self._procedures_by_name.items():
                 puts("{0}:".format(name))
                 with indent(2):
                     result = self._run_procedure(name, procedure_cls, target_dir)
-                    if result > 0:
-                        return result
-                    elif result > -1:
-                        successful_procedures += 1
-        puts(colored.green("Complete! {0} procedure(s) performed.".format(successful_procedures)))
+                    results[result] += 1
+                    if not ignore_errors and result == Result.FAIL:
+                        with indent(-4):
+                            puts(colored.red("Surgen ending early"))
+                            self._print_results(results)
+                        return 1
+        self._print_results(results)
+        return 1 if Result.FAIL in results else 0
+
+    def _print_results(self, results):
+        puts(colored.green("Complete! {0} procedure(s) performed.".format(len(self._procedures_by_name))))
+        puts(colored.green("{0} success, {1} failed, {2} skipped".format(*[results[r] for r in Result.__members__.values()])))
 
     def _run_procedure(self, name, procedure_cls, target_dir):
         puts(colored.yellow("executing...".format(name)))
@@ -38,15 +51,16 @@ class Surgen(object):
         should_not_run_reason = procedure.should_not_run()
         if should_not_run_reason:
             puts(colored.yellow("skipping, should_not_run returned: {0}".format(should_not_run_reason)))
-            return 0
+            return Result.SKIP
         try:
             with indent(2):
                 result = procedure.operate()
         except Exception as e:
             LOG.debug("", exc_info=True)
             puts(colored.red("procedure raised an exception! {0}".format(e)))
-            return 1
+            return Result.FAIL
         puts(colored.green("complete!"))
+        return Result.PASS
 
 
 def surgen_from_directory(procedure_dir):
